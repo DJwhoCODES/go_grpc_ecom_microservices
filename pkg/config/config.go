01a55eb4ce98819aa67b2e004a09279504560773
DJwhoCODES <jaindevanshu824@gmail.com>
2026-03-15 18:07:50 +0530

    Env Configs and Load file

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,44 @@
 package config
+
+import "os"
+
+type Config struct {
+	ServiceName string
+	ServicePort string
+
+	PostgresHost     string
+	PostgresPort     string
+	PostgresUser     string
+	PostgresPassword string
+	PostgresDB       string
+
+	RedisHost string
+	RedisPort string
+}
+
+func Load() *Config {
+	return &Config{
+		ServiceName: getEnv("SERVICE_NAME", "unknown-service"),
+		ServicePort: getEnv("SERVICE_PORT", "50051"),
+
+		PostgresHost:     getEnv("POSTGRES_HOST", "localhost"),
+		PostgresPort:     getEnv("POSTGRES_PORT", "5432"),
+		PostgresUser:     getEnv("POSTGRES_USER", "postgres"),
+		PostgresPassword: getEnv("POSTGRES_PASSWORD", "postgres"),
+		PostgresDB:       getEnv("POSTGRES_DB", "ecommerce"),
+
+		RedisHost: getEnv("REDIS_HOST", "localhost"),
+		RedisPort: getEnv("REDIS_PORT", "6379"),
+	}
+}
+
+func getEnv(key string, fallback string) string {
+
+	value, exists := os.LookupEnv(key)
+
+	if !exists {
+		return fallback
+	}
+
+	return value
+}
