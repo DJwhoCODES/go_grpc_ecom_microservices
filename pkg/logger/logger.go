a0ddca1c48245e73efbed01024ce9b1bac5c5a01
DJwhoCODES <jaindevanshu824@gmail.com>
2026-03-15 17:55:55 +0530

    configured logger by zap

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,35 @@
 package logger
+
+import "go.uber.org/zap"
+
+var Log *zap.Logger
+
+func Init() {
+	var err error
+
+	config := zap.NewProductionConfig()
+
+	config.Encoding = "json"
+
+	Log, err = config.Build()
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Sync() {
+	_ = Log.Sync()
+}
+
+func Info(msg string, fields ...zap.Field) {
+	Log.Info(msg, fields...)
+}
+
+func Error(msg string, fields ...zap.Field) {
+	Log.Error(msg, fields...)
+}
+
+func Fatal(msg string, fields ...zap.Field) {
+	Log.Fatal(msg, fields...)
+}
